cee2a7332d3b1f8f7ea2ccd206c9104fdd32e776
violin788 <violin78@protonmail.com>
2025-03-14 19:27:58 -0400

    Your commit message here

--- 0_20_earnings.py
+++ 0_20_earnings.py
@@ -7,9 +7,12 @@ edgar_list = os.listdir(edgar_folder)
 
 for stock in edgar_list:
     k8_folder = os.path.join(edgar_folder,stock,"8-K")
-    k8_date_list = os.listdir(k8_folder)
+    try:
+        k8_date_list = os.listdir(k8_folder)
+    except:
+        continue
     number_of_dates = len(k8_date_list)
-    if number_of_dates>20:
-        print(number_of_dates,stock)
+    if number_of_dates<20:
         folder_delete = os.path.join(edgar_folder,stock)
+        print(number_of_dates,stock,"will delete = "+folder_delete)
         shutil.rmtree(folder_delete)
