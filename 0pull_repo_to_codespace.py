775f67d94e76fef53e2e15acde24a08f64526203
violin788 <violin78@protonmail.com>
2025-03-15 14:43:57 +0000

    Updated files

--- 0pull_repo_to_codespace.py
+++ 0pull_repo_to_codespace.py
@@ -2,7 +2,10 @@ import subprocess
 # Example command to run in the terminal
 #command = "ls"  # List directory contents (replace with any terminal command)
 command = """
-git pull origin main
+git reset --hard
+git fetch --all
+git reset --hard origin/main
+git status
 """
 result = subprocess.run(command, shell=True, text=True, capture_output=True)
 # Print the output of the command
