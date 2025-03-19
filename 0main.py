d859637fcc22b617f75106ebe80a469309c7eec2
violin788 <violin78@protonmail.com>
2025-03-19 16:31:13 +0000

    Updated files

--- 0main.py
+++ 0main.py
@@ -577,4 +577,4 @@ prices_around_earnings(upcoming_file,required_ratio)
 """
 #specific_day(start_date,end_date, match_file)
 """
-#last updated=2025-03-19 14:44:27----------
+#last updated=2025-03-19 16:31:13----------
