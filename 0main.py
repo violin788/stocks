ccd47d14b6994746935dfdfb933e2000ce27200c
violin788 <violin78@protonmail.com>
2025-03-19 21:01:28 +0000

    updated: 2025-03-19 21:01:28

--- 0main.py
+++ 0main.py
@@ -608,4 +608,4 @@ prices_around_earnings(upcoming_file,required_ratio,folder_analysis)
 """
 #specific_day(start_date,end_date, match_file)
 """
-#last updated=2025-03-19 21:01:24----------
+#last updated=2025-03-19 21:01:28----------
